85bf8d8606712b8202f3ff7d8691cd2d137c0570
Darin Hullinger <doogie540@yahoo.com>
2020-06-19 14:40:00 -0600

    fixed issue #1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            SaySomething();
+            await SaySomething();
             Console.WriteLine(result);
             Console.WriteLine("stuff");
         }
