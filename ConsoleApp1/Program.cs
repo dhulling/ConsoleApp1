2a4266689e9e502c07e2051dd8855fac60458eae
Darin Hullinger <doogie540@yahoo.com>
2020-06-19 14:04:39 -0600

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string result;
+
+        static void Main(string[] args)
+        {
+            SaySomething();
+            Console.WriteLine(result);
+            Console.WriteLine("stuff");
+        }
+        static async Task<string> SaySomething()
+        {
+            await Task.Delay(5);
+            result = "Hello World";
+            return "Something";
+        }
+    }
+}
